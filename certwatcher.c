14c350e6bb7d9103dcaf00dd2268ca94b589a2a9
chillymasterio <chillymasterio@gmail.com>
2026-05-16 23:35:05 +0300

    Add --issuer-filter to show only certs from specific CA

--- certwatcher.c
+++ certwatcher.c
@@ -64,6 +64,7 @@ static const char *g_exclude_pattern = NULL;
 static int g_read_stdin = 0;
 static int g_connect_timeout = 10;
 static int g_revoked_only = 0;
+static const char *g_issuer_filter = NULL;
 static const char *g_date_format = "%Y-%m-%d %H:%M:%S UTC";
 
 #define C_RED     (use_color ? "\033[31m" : "")
